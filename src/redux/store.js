1839131e084e37480308e07365fe3c10215bd98e
fairskyDev1108 <BogumirMysliwiecDevb@outlook.com>
2022-01-06 07:40:16 +0300

    axios add. Router using. pages/Home add. created characterSlice.js. getCharacters()

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import charactersSlice from "./charactersSlice.js";
+
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        characters: charactersSlice
+    }
 })
