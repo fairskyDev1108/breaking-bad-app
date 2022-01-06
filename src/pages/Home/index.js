55559a16a1e43606f738f05a6499e65d6653b28f
fairskyDev1108 <BogumirMysliwiecDevb@outlook.com>
2022-01-06 21:18:31 +0300

    Added Character List. Created error and loading components.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,41 @@
+//react-mesonory-css kullanılabilir
 import React,{useEffect} from 'react'
-
 import {useSelector, useDispatch} from "react-redux"
 import {getCharacters} from "../../redux/charactersSlice"
+import Loading from '../../Components/Loading'
+import Error from '../../Components/Error'
+
+import './style.css'
 
 function Home() {
-    const data = useSelector((state) => state.characters)
-    const dispatch = useDispatch()
+    const characters = useSelector((state) => state.characters.items)
+    const isLoading  = useSelector((state) => state.characters.isLoading)
+    const error      = useSelector((state) => state.characters.error)
 
+    const dispatch = useDispatch()
+    console.log(characters);
+    
     useEffect(() =>{
         dispatch(getCharacters())
     },[])
+
+    if(isLoading){
+        return <Loading />
+    }
+    if(error){
+        return <Error message={error}/>
+    }
+
     return (
-        <div>
-            Home
+        <div className='cards'>
+            {
+                characters.map(item => (
+                    <div className='card'>
+                        <img src={item.img}/>
+                        <h3 className='name'>{item.name}</h3>
+                    </div>
+                ))
+            }
         </div>
     )
 }
--- src/redux/charactersSlice.js
+++ src/redux/charactersSlice.js
@@ -1,8 +1,9 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit"
 import axios from "axios";
 
+const CHAR_LIMIT = 12
 export const getCharacters = createAsyncThunk('characters/getCharacters', async () => {
-    const res = await axios("https://breakingbadapi.com/api/characters?limit=10")
+    const res = await axios(`https://breakingbadapi.com/api/characters?limit=${CHAR_LIMIT}`)
     return res.data
 })
 
@@ -10,11 +11,20 @@ export const charactersSlice = createSlice({
     name:"characters",
     initialState: {
         items: [],
+        isLoading: false,
     },
     reducers: {},
     extraReducers:{
+        [getCharacters.pending]: (state, action) => {
+            state.isLoading = true
+        },
         [getCharacters.fulfilled]: (state, action) => {
-            console.log(action.payload);
+            state.items = action.payload
+            state.isLoading = false
+        },
+        [getCharacters.rejected]: (state, action) => {
+            state.isLoading = false
+            state.error = action.error.message
         }
     }
 });
